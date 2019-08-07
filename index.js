dac8551ab7042045b386166970f1a24813ac811b
Mantej Singh <749948@student.tor.loc>
2019-08-07 17:17:12 -0400

    last commit

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var app = express()
-
 app.get('/',function(req,res){
+    res.send('<h1>HELLo WORLD</h1>')
+    })
+
+app.post('/',function(req,res){
 res.send('HELL WORLD')
 })
+app.put('/',function(req,res){
+    res.send('putttt WORLD')
+    })
+    app.delete('/',function(req,res){
+        res.send('putttt WORLD')
+        })
 
 app.listen(3000)
