d972f99088bb4373af9f5aecb59373ab35cd9f01
Mantej Singh <749948@student.tor.loc>
2019-08-07 15:51:22 -0400

    third commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const data = {
+    firstName:'Laurence',
+    lastName:'Svekis'
+}
+
+const site = http.createServer(function(req,res){
+    console.log('Hello World');
+    console.log(req.headers);
+    Console.log(req.url);
+    res.setHeader('Content-Type','application/json');
+    res.write(JSON.stringify(data));
+    res.end();
+});
+
+site.listen(3000);
